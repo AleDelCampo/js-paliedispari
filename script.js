8ed6b7a992df1cf47aa74955be417c7ffe6387c9
AleHashirama <alehashirama@gmail.com>
2024-02-09 18:41:39 +0100

    cambiata la modalità di calcolo somma perchè notavo che vinceva sempre il computer

--- script.js
+++ script.js
@@ -1,48 +1,31 @@
 function evenOrOdd() {
-
     const userChoice = prompt("Pari o Dispari:");
 
     if (userChoice !== "Pari" && userChoice !== "Dispari") {
-
         alert("Scelta non valida. Scegli tra pari o dispari.");
-
         return;
-    };
+    }
 
-    const userNumber = parseInt(prompt("Ora Tira!!(un numero da 1 a 5)"));
+    const userNumber = parseInt(prompt("Ora Tira!! (un numero da 1 a 5)"));
 
     if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
-
         alert("Numero non valido. Inserisci un numero da 1 a 5.");
-
         return;
-    };
+    }
 
     function randomNumber() {
-
         return Math.floor(Math.random() * 5) + 1;
-    
     }
     
     const computerNumber = randomNumber();
+    const sum = userNumber + computerNumber;
+    const evenSum = sum % 2 === 0;
 
-    function somma(userNumber, computerNumber) {
-
-        return userNumber + computerNumber;
-
-    }
-
-    const sum = somma(userNumber, computerNumber);
-
-    if (sum === userChoice) {
-
-        document.getElementById("result").innerHTML = ("La vittoria va all'User.!");
-
+    if ((userChoice === "Pari" && evenSum) || (userChoice === "Dispari" && !evenSum)) {
+        document.getElementById("result").innerHTML = "La vittoria va all'User!!";
     } else {
-
-        document.getElementById("result").innerHTML = ("La tecnologia Vince.");
-
-    };
-};
+        document.getElementById("result").innerHTML = "La tecnologia Vince.";
+    }
+}
 
 evenOrOdd();
